15946ebe2a7c530671f584d2f0405dec31b0ad89
najwanzaky <najwanzaky21@gmail.com>
2023-05-11 02:55:40 +0700

    update cekPemenang

--- najwan.c
+++ najwan.c
@@ -30,7 +30,7 @@ void swapScore(Isi_Team R, int Jml_Tim) {
 	team temp2;
 	for(k = 0; k < Jml_Tim/4; k++) { //perulangan dengan syarat berikut bertujuan untuk membagi setiap grup terdiri dari 4 tim
 		for(i = 1 + k*4; i <= 4 + k*4; i++) { //variabel i pada perulangan berikut berguna untuk menjadi indeks pertama yang akan dibandingkan. nilai awal k adalah 0, jadi batasan pertamanya adalah 4
-			for(j = i+1; j <= 4 + k*4; j++) { //variabel j pada perulangan berikut berguna untuk menjadi indeks kedua yang akan dibandingkan dengan indeks pertama. 
+			for(j = i+1; j <= 4 + k*4; j++) { //variabel j pada perulangan berikut berguna untuk menjadi indeks kedua yang akan dibandingkan dengan indeks pertama.
 				if(score[i] < score[j]) { //jika indeks variabel i lebih kecil dari indeks variabel j, maka akan melakukan swap/penukaran
 					temp = score[i];      //melakukan penukaran posisi score. score pertama (i) akan disimpan sementara pada temp
 					score[i] = score[j];  //score kedua (j) ditempatkan ke posisi pertama
@@ -59,34 +59,61 @@ void sortByScore(Isi_Team R, int Jml_Tim) {
 }
 
 void cekPemenang(Isi_Team Q, int Jml_Tim, Isi_Team R, char timPemenang[][500]) {
-	int i, j, k, l;
+	int i = 1, j = 2, k = 3, l = 4;
 	int x = 0, grup;
 	char ulang;
 	int index = 0;
-	for(l = 0; l < Jml_Tim/4; l++) {
-		printf("\nGrup %c\n", 'A' + l);
+	for(x = 0; x < Jml_Tim/4; x++) {
+		printf("\nGrup %c\n", 'A' + x);
 		printf("Tim yang masuk ke babak Gugur: \n");
 		printf("--------------------\n");
-		for (i = 1 + l*4; i <= 4 + l*4; i++) {
-			for(j = i+1; j <= 4 + l*4; j++) {
-				for(k = j+1; k <= 4 + l*4; k++) {
-					if(Q[i].score == Q[j].score == Q[k].score && Q[i].score == Q[k].score != Q[j].score || Q[i].score != Q[k].score == Q[j].score) {
-							for (i = 1 + l*4; i <= 2 + l*4; i++) {
-								printf("%s\n", R[i].name);
-								strcpy(timPemenang[index], R[i].name);
-								index++;
-								x = 1;
-							}
-						}
-					}
-				}
-			}
-		}
+		i = 1 + x*4;
+		j = 2 + x*4;
+		k = 3 + x*4;
+		l = 4 + x*4;
 
-		if( x != 1) {
-			getPemenang(Q, Jml_Tim, timPemenang);
+		if(Q[i].score == Q[j].score && Q[i].score == Q[k].score && Q[i].score == Q[l].score && Q[j].score == Q[l].score && Q[j].score == Q[k].score && Q[k].score == Q[l].score ) {
+			printf("%s\n", Q[i].name);
+			printf("%s", Q[i+1].name);
+			strcpy(timPemenang[index],Q[i].name);
+			index++;
+			strcpy(timPemenang[index],Q[i+1].name);
+			index++;
+		} else if(Q[i].score == Q[j].score == Q[k].score && Q[k].score != Q[l].score) {
+			
+			printf("%s\n", R[i].name);
+			printf("%s\n", R[i+1].name);
+			strcpy(timPemenang[index],R[i].name);
+			index++;
+			strcpy(timPemenang[index],R[i+1].name);
+			index++;
+				
+		} else if(Q[j].score == Q[k].score && Q[k].score == Q[l].score && Q[i].score!= Q[j].score && Q[i].score!= Q[k].score && Q[i].score!= Q[l].score ) {
+			printf("%s\n", Q[i].name);
+			printf("%s", Q[i+1].name);
+			strcpy(timPemenang[index],Q[i].name);
+			index++;
+			strcpy(timPemenang[index],Q[i+1].name);
+			index++;
+		} else if(Q[i].score!= Q[j].score && Q[i].score!= Q[k].score && Q[i].score!= Q[l].score && Q[j].score!= Q[k].score && Q[j].score!= Q[l].score && Q[l].score!= Q[k].score || Q[l].score== Q[k].score){
+			printf("%s\n", Q[i].name);
+			printf("%s", Q[i+1].name);
+			strcpy(timPemenang[index],Q[i].name);
+			index++;
+			strcpy(timPemenang[index],Q[i+1].name);
+			index++;
+		} else if(Q[i].score == Q[j].score){
+			printf("%s\n", Q[i].name);
+			printf("%s", Q[i+1].name);
+			strcpy(timPemenang[index],Q[i].name);
+			index++;
+			strcpy(timPemenang[index],Q[i+1].name);
+			index++;
 		}
 	}
+
+
+}
 
 
 void inputSkor(Isi_Team Q, int Jml_Tim, int scoreI, int scoreJ) {
@@ -103,11 +130,11 @@ void inputSkor(Isi_Team Q, int Jml_Tim, int scoreI, int scoreJ) {
 
 				score[i] = scoreI + score[i];
 				score[j] = scoreJ + score[j];
-				
-				if(scoreI < 0 || scoreJ < 0){
+
+				if(scoreI < 0 || scoreJ < 0) {
 					printf("Invalid input");
 					j--;
-					continue;	
+					continue;
 				}
 				if(scoreI > scoreJ) {
 					Q[i].score = Q[i].score + 3;
@@ -117,27 +144,26 @@ void inputSkor(Isi_Team Q, int Jml_Tim, int scoreI, int scoreJ) {
 					Q[i].score = Q[i].score + 1;
 					Q[j].score = Q[j].score + 1;
 				}
-				
+
 			}
 		}
 	}
 }
 
-void aduPenalti(Isi_Tree P, int i, int index, char timPemenangTree[][500]){
+void aduPenalti(Isi_Tree P, int i, int index, char timPemenangTree[][500]) {
 	int scoreI,scoreJ;
 	printf("\n ADU PENALTI ANTARA %s DAN %s", P[i].nama, P[i+1].nama);
-	for(int kicker=1; kicker<=5; kicker++){
+	for(int kicker=1; kicker<=5; kicker++) {
 		printf("Tendangan ke: %d\n", kicker);
 		do {
 			printf("%s: ", P[i].nama);
 			scanf("%d", &scoreI);
 			printf("%s: ", P[i+1].nama);
 			scanf("%d", &scoreJ);
-			
-			if ((scoreI != 0 && scoreI != 1) || (scoreJ != 0 && scoreJ != 1)){
+
+			if ((scoreI != 0 && scoreI != 1) || (scoreJ != 0 && scoreJ != 1)) {
 				printf("Invalid input! skor harus 0 atau 1.\n");
-			}
-			else {
+			} else {
 				break;
 			}
 		} while (1);
@@ -145,12 +171,12 @@ void aduPenalti(Isi_Tree P, int i, int index, char timPemenangTree[][500]){
 		scorePenalti[i] += scoreI;
 		scorePenalti[i+1] += scoreJ;
 	}
-	
-	if(scorePenalti[i] == scorePenalti[i+1]){
+
+	if(scorePenalti[i] == scorePenalti[i+1]) {
 		aduPenalti(P, i, index, timPemenangTree);
-	}else if(scorePenalti[i] < scorePenalti[i+1]){
+	} else if(scorePenalti[i] < scorePenalti[i+1]) {
 		strcpy(timPemenangTree[index], P[i+1].nama);
-	}else if(scorePenalti[i] > scorePenalti[i+1]){
+	} else if(scorePenalti[i] > scorePenalti[i+1]) {
 		strcpy(timPemenangTree[index], P[i].nama);
 	}
 }
